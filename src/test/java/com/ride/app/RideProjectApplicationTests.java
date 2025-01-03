7d181071c9a9fd1a85515becae09e6294eb4613d
jyt <dudxoor68@naver.com>
2025-01-03 15:05:59 +0900

    Initial commit

---
+++ src/test/java/com/ride/app/RideProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ride.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
